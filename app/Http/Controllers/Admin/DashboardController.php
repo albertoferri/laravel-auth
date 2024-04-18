ee7b4c7f16eedace88942137385624289cb3acb9
albertoferri <falberto99@gmail.com>
2024-04-18 18:30:52 +0200

    aggiunta lista progetti nella admin.indexc ma non visualizzabile

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Project;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('admin.index');
+        $projects = Project::all();
+        return view('admin.index', compact('projects'));
     }
 
     public function users() {
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,6 +6,21 @@
 
     <h1>DASHBOARD DI AMMINISTRAZIONE</h1>
 
+    <div class="row row-gap-4">
+        @foreach ($projects as $project)
+        <div class="card mb-3">
+            <img src="{{$project->thumb}}" class="card-img-top" alt="immagine progetto">
+            <div class="card-body">
+              <h5 class="card-title">{{$project->name}}</h5>
+              <p class="card-text">{{$project->description}}</p>
+              <p class="card-text">{{$project->skill}}</p>
+              <p class="card-text">{{$project->git_url}}</p>
+              <a href="{{route('project.show', $project->id)}}" class="btn btn-success fw-bold text-uppercase">maggiori informazioni</a>
+            </div>
+        </div>
+        @endforeach
+    </div>
+
 </div>
 
 @endsection
